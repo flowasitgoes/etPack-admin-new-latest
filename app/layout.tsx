8e197f73331e75774da7105eb593ff255957c97d
Flow <guanjun211@gmail.com>
2025-07-21 15:14:14 +0800

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EtPack ERP Admin',
+  description: 'EtPack ERP Admin 07212025',
+  generator: 'v+c',
 }
 
 export default function RootLayout({
